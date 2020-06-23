8cf9ca8cc0113e7521786dde5cffe5fdd8689c8b
Filipe <filipeccm@gmail.com>
2020-06-23 16:25:57 +0200

    Updates

---
+++ precache-manifest.dea561edc5217a21595d8082d6373cf7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509693a04d8d14db35d04ab980c84bda",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "28c4f40cda6c522c5f98",
+    "url": "/weather-api/static/css/main.3d3e92f1.chunk.css"
+  },
+  {
+    "revision": "16c1e43b8b3096050090",
+    "url": "/weather-api/static/js/2.254f270f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-api/static/js/2.254f270f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c4f40cda6c522c5f98",
+    "url": "/weather-api/static/js/main.cec2b3be.chunk.js"
+  },
+  {
+    "revision": "333dd0da4143220dee8c",
+    "url": "/weather-api/static/js/runtime-main.a3d81847.js"
+  },
+  {
+    "revision": "dca8ccdaed9d7f4d8a4c6b2152367c3d",
+    "url": "/weather-api/static/media/clouds.dca8ccda.svg"
+  },
+  {
+    "revision": "dff0cd2c4e6451f673fbf435612ec00a",
+    "url": "/weather-api/static/media/humidity.dff0cd2c.svg"
+  },
+  {
+    "revision": "88f62097d661e4bffc684f8750556f3a",
+    "url": "/weather-api/static/media/mist.88f62097.svg"
+  },
+  {
+    "revision": "6ff26a69b1a0b28e66dc5bd2b86cfd05",
+    "url": "/weather-api/static/media/rain.6ff26a69.svg"
+  },
+  {
+    "revision": "1d5e825b4d0c6e441d656072dc99de66",
+    "url": "/weather-api/static/media/snow.1d5e825b.svg"
+  },
+  {
+    "revision": "303aaab9b789652d1b5a60e85d62582a",
+    "url": "/weather-api/static/media/sunny.303aaab9.svg"
+  },
+  {
+    "revision": "c97bdf480a90b6c529197d4fe7efa49d",
+    "url": "/weather-api/static/media/wind.c97bdf48.svg"
+  }
+]);
